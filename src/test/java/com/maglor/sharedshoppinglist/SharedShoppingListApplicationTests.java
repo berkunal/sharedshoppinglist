c6ef9ffd48ee816760c33077b25dfc98ec6170c7
Berk Unal <cesmeli.berk@gmail.com>
2021-11-18 22:57:27 +0300

    Initial commit

---
+++ src/test/java/com/maglor/sharedshoppinglist/SharedShoppingListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maglor.sharedshoppinglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedShoppingListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
