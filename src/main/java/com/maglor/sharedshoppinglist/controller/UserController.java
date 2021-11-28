6b17e3c01f00cfb1ce353eb3c8e5ba1442cea0b2
Berk Unal <cesmeli.berk@gmail.com>
2021-11-28 16:40:39 +0300

    Introduce User service and controller

---
+++ src/main/java/com/maglor/sharedshoppinglist/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.maglor.sharedshoppinglist.controller;
+
+import com.maglor.sharedshoppinglist.model.User;
+import com.maglor.sharedshoppinglist.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping()
+    public List<User> getUsers() {
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable String id) {
+        return userService.getUserById(UUID.fromString(id));
+    }
+
+    @PostMapping()
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @PutMapping()
+    public User updateUser(@RequestBody User user) {
+        return userService.updateUser(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable String id) {
+        userService.deleteUser(UUID.fromString(id));
+    }
+}
---
+++ src/main/java/com/maglor/sharedshoppinglist/service/UserService.java
@@ -0,0 +1,37 @@
+package com.maglor.sharedshoppinglist.service;
+
+import com.maglor.sharedshoppinglist.model.User;
+import com.maglor.sharedshoppinglist.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(UUID id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User updateUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(UUID id) {
+        userRepository.deleteById(id);
+    }
+
+}
